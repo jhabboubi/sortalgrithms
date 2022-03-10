5aa33377da296eda9bb35bc482337a1e9b5a8d70
Jafer Alhaboubi <habboubi@gmail.com>
2022-03-10 07:25:21 -0600

    Tests Cleanup / added three second timeout

--- src/test/java/john/with/code/SortAlgorithmsTest.java
+++ src/test/java/john/with/code/SortAlgorithmsTest.java
@@ -1,48 +1,53 @@
 package john.with.code;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SortAlgorithmsTest {
+    Random r;
+    int[] arr;
+    int[] arrCopy;
+
+    @BeforeEach
+    void setUp() {
+        r = new Random();
+        arr = new int[10000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(Integer.MAX_VALUE);
+        }
+        arrCopy = Arrays.copyOf(arr, arr.length);
+
+    }
 
     @Test
+    @Timeout(3)
     void quicksort() {
-        Random r = new Random();
-        int[] arr = new int[100000];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]=r.nextInt(100000);
-        }
-        int[] arrCopy = Arrays.copyOf(arr,arr.length);
         Arrays.sort(arrCopy);
         SortAlgorithms.quicksort(arr);
         assertArrayEquals(arrCopy, arr);
+    }
+
+    @Test
+    @Timeout(3)
+    void bubbleSort() {
+        Arrays.sort(arrCopy);
+        assertArrayEquals(new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90}, SortAlgorithms.bubbleSort(new int[]{80, 70, 10, 30, 50, 20, 90, 40, 60}));
+        assertArrayEquals(arrCopy, SortAlgorithms.bubbleSort(arr));
 
     }
 
     @Test
     void swap() {
-        Object[] arr = new Object[]{10,20,30,40,50,60,70,80,90};
-        SortAlgorithms.swap(arr,0,arr.length-1);
-        assertArrayEquals(new Object[]{90,20,30,40,50,60,70,80,10},arr);
-
+        Object[] arr = new Object[]{10, 20, 30, 40, 50, 60, 70, 80, 90};
+        SortAlgorithms.swap(arr, 0, arr.length - 1);
+        assertArrayEquals(new Object[]{90, 20, 30, 40, 50, 60, 70, 80, 10}, arr);
     }
 
-    @Test
-    void bubbleSort() {
-        Random r = new Random();
-        int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]=r.nextInt(10000000);
-        }
-        int[] arrCopy = Arrays.copyOf(arr,arr.length);
-        Arrays.sort(arrCopy);
-        assertArrayEquals(new int[]{10,20,30,40,50,60,70,80,90}, SortAlgorithms.bubbleSort(new int[]{80,70,10,30,50,20,90,40,60}));
-        assertArrayEquals(arrCopy, SortAlgorithms.bubbleSort(arr));
 
-    }
 }
